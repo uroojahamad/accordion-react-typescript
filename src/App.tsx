be4a255285cd3d9f733d9608c85108bf5b4e7b07
Urooj Ahmad <uroojahamad786@gmail.com>
2023-06-16 14:31:31 +0530

    added functionality to open accordion one at time

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import {useState} from 'react';
 import Accordion from './components/Accordion';
 
+type ObjectType = {
+  title: String;
+  description: String;
+};
+
+type ArrayType = Array<ObjectType>;
+
+const accordionInfo : ArrayType= [
+  {
+    title:'The Flash',
+    description:'After being struck by lightning, Barry Allen wakes up from his coma to discover he been given the power of super speed, becoming the Flash'
+  },
+  {
+    title:'Spiderman',
+    description:'Peter Parker then attains spider-like qualities such as genius-level intellect, superb scientist and inventor, superhuman strength, speed, durability, and agility, he has the ability to cling to most surfaces, precognitive spider-sense, and his wrist as web-shooters.'
+      
+  },
+  {
+    title:'Ironman',
+    description:'A brash but brilliant inventor, Stark was self-described as a genius, billionaire, playboy, and philanthropist.'
+      
+  }
+];
+
 function App() {
+  const [openIndex, setOpenIndex] = useState(-1);
+  const handleAccordionClick = (index: number) => {
+    setOpenIndex((prevIndex) => {
+      if (prevIndex === index) {
+        // we have clicked twice
+        return -1;
+      } else {
+        // we have clicked for first time
+        return index;
+      }
+    });
+  };
   return (
     <>
-      <Accordion 
-        title='The Flash'
-        content='After being struck by lightning, Barry Allen wakes up from his coma to discover he been given the power of super speed, becoming the Flash'
-      />
-      <Accordion 
-        title='Spiderman'
-        content='Peter Parker then attains spider-like qualities such as genius-level intellect, superb scientist and inventor, superhuman strength, speed, durability, and agility, he has the ability to cling to most surfaces, precognitive spider-sense, and his wrist as web-shooters.'
-      />
-      <Accordion 
-        title='Ironman'
-        content='A brash but brilliant inventor, Stark was self-described as a genius, billionaire, playboy, and philanthropist.'
-      />
+      {accordionInfo.map((item, index) => (
+        <Accordion
+          key={index}
+          index={index}
+          title={item.title}
+          description={item.description}
+          isOpen={openIndex === index}
+          handleAccordionClick={handleAccordionClick}
+        />
+      ))}
     </>
   );
 }
